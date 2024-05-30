7026b9689573f1e0d18eda20757c472d2f7cc231
Chris Lewis <>
2024-05-30 22:15:55 +0200

    work on archetypes

---
+++ archetype.py
@@ -0,0 +1,34 @@
+class Archetype:
+    def __init__(self, name: str, mandatoryTypes: list, coreMinions: list, tier: int, optionalTypes = [], description = "", optionalMinions = []):
+     self.name = name
+     self.mandatoryTypes = mandatoryTypes
+     self.optionalTypes = optionalTypes
+     self.description = description
+     self.coreMinions = coreMinions
+     self.optionalMinions = optionalMinions
+     self.tier = tier
+
+    def printArchetypeInfo(self):
+        print(f'{self.name}\n{self.description}')
+        minionsByTier = {}
+        allMinions = self.coreMinions + self.optionalMinions
+        allMinions.sort(key=lambda minion: minion.tier)
+        for minion in allMinions:
+            if minion.tier in minionsByTier:
+                minionsByTier[minion.tier].append(minion)
+            else:
+                minionsByTier[minion.tier] = [minion]
+        for i in minionsByTier:
+            coreList = []
+            optionalList = []
+            for minion in minionsByTier[i]:
+                if minion in self.coreMinions:
+                    coreList.append(minion)
+                elif minion in self.optionalMinions:
+                    optionalList.append(minion)
+            if len(coreList) + len(optionalList) > 0:
+                print(f'level {i}')
+                if len(coreList) > 0:
+                    print (f'core minions: {', '.join([minion.name for minion in coreList])}')
+                if len(optionalList) > 0:
+                    print (f'optional minions: {', '.join([minion.name for minion in optionalList])}')
--- cardDatabase.py
+++ cardDatabase.py
@@ -1,7 +1,10 @@
 import json
 import requests
 import os
+import archetype
 
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
 from enum import Enum
 from requests.auth import HTTPBasicAuth
 from dotenv import load_dotenv
@@ -19,15 +22,9 @@ class CreatureType(Enum):
     QUILLBOAR = 43
     NAGA = 92
 
-class Archetypes:
-    def __init__(self, name: str, mandatoryTypes: list, minions: list, level: int, optionalTypes = {}, description = ""):
-     self.name = self.__class.__name__
-     self.mandatoryTypes = mandatoryTypes
-     self.optionalTypes = optionalTypes
-     self.description = description
-     self.minions = minions
-     self.level = level
-    pass
+
+                    
+
 
 class Card:
     def __init__(self, name, types, tier, image, duosOnly, solosOnly, id):
@@ -41,37 +38,67 @@ class Card:
         self.id = id
 
 
-def get_api_data():
-    client_id = os.environ.get("CLIENT_ID")
-    client_secret = os.environ.get("CLIENT_SECRET")
-    token_response = requests.post(url = "https://oauth.battle.net/token", data={"grant_type" : "client_credentials"}, auth = HTTPBasicAuth(client_id, client_secret))
-    token = token_response.json()["access_token"]
-    url = "https://api.blizzard.com/hearthstone/cards?bgCardType=minion&gameMode=battlegrounds&pageSize=1000&locale=en_US"
-    return requests.get(url = url, headers={"authorization" : f"Bearer {token}" }).json()
+class CardDatabase:
 
-print("program running")
+    def __init__(self):
+        self.battlegrounds_card_dict = {}
+        cards_list = self.get_api_data()["cards"]
+
+        for card in cards_list:
+            types = []
+            if "minionTypeId" in card:
+                types.append(CreatureType(card["minionTypeId"]))
+            if "multiTypeIds" in card:
+                types += [CreatureType(type) for type in card["multiTypeIds"]]
+            if len(types) == 0:
+                types.append(CreatureType.ALL)
+            battlegrounds = card["battlegrounds"]
+            local_card = Card(
+                name= card["name"],
+                types= types,
+                tier= battlegrounds["tier"],
+                image=battlegrounds["image"],
+                duosOnly=battlegrounds["duosOnly"],
+                solosOnly=battlegrounds["solosOnly"],
+                id= card["id"]
+                            )
+            self.battlegrounds_card_dict[local_card.name] = local_card
+
+
+    def get_api_data(self):
+        client_id = os.environ.get("CLIENT_ID")
+        client_secret = os.environ.get("CLIENT_SECRET")
+        token_response = requests.post(url = "https://oauth.battle.net/token", data={"grant_type" : "client_credentials"}, auth = HTTPBasicAuth(client_id, client_secret))
+        token = token_response.json()["access_token"]
+        url = "https://api.blizzard.com/hearthstone/cards?bgCardType=minion&gameMode=battlegrounds&pageSize=1000&locale=en_US"
+        return requests.get(url = url, headers={"authorization" : f"Bearer {token}" }).json()
+    
+    def getMinion(self,name: str):
+        l_name = name.lower()
+        fuzzy = process.extract(name, self.battlegrounds_card_dict.keys(), limit = 1)
+        fuzzyName = fuzzy[0][0]
+
+        if fuzzyName in self.battlegrounds_card_dict:
+            return (self.battlegrounds_card_dict[fuzzyName])
+        
+    def getMinions(self,*names: str):
+        minions = []
+        for name in names:
+            minions.append(self.getMinion(name))
+        return minions
+
 if __name__ == "__main__":
     load_dotenv()
+    cardDatabase = CardDatabase()
 
-    battlegrounds_card_dict = {}
-    cards_list = get_api_data()["cards"]
+    undead_attack_stacking = archetype.Archetype(
+        name= "Undead Attack Stacking",
+        mandatoryTypes=[CreatureType.UNDEAD],
+        optionalTypes=[CreatureType.BEAST, CreatureType.MECH, CreatureType.MURLOC],
+        coreMinions=cardDatabase.getMinions("Archlinch Kel'Thuzad", "hateful hag", "handless forsaken"),
+        optionalMinions=cardDatabase.getMinions("nerubian deathswarmer", "murgl mk II"),
+        tier=5,
+        description="Stack permanent attack buffs on your undead, use deathrattle and reborn to attack numerous times."
+    )
 
-    for card in cards_list:
-        types = []
-        if "minionTypeId" in card:
-            types.append(CreatureType(card["minionTypeId"]))
-        if "multiTypeIds" in card:
-            types += [CreatureType(type) for type in card["multiTypeIds"]]
-        if len(types) == 0:
-            types.append(CreatureType.ALL)
-        battlegrounds = card["battlegrounds"]
-        local_card = Card(
-            name= card["name"],
-            types= types,
-            tier= battlegrounds["tier"],
-            image=battlegrounds["image"],
-            duosOnly=battlegrounds["duosOnly"],
-            solosOnly=battlegrounds["solosOnly"],
-            id= card["id"]
-                        )
-        battlegrounds_card_dict[local_card.name] = local_card
+    undead_attack_stacking.printArchetypeInfo()
